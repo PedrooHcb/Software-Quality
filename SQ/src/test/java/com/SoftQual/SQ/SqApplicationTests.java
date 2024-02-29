babed3b5a7fb70ddcdbce55f342aeb4fb4aa100d
Pedro Barros <pedro.hcb@live.com>
2024-02-28 21:02:54 -0300

    first commit

---
+++ SQ/src/test/java/com/SoftQual/SQ/SqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftQual.SQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
